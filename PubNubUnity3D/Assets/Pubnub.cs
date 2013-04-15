aba13cfeb23a69d5fa4e181bd7ac904d33822779
Roy Davies <roy.c.davies@imersia.com>
2013-04-16 09:06:26 +1200

    Changed pubnub.cs script to allow more connections

--- PubNubUnity3D/Assets/Pubnub.cs
+++ PubNubUnity3D/Assets/Pubnub.cs
@@ -1,6 +1,6 @@
 // ******************************************************************************************************************************************************
 // Copyright Imersia Ltd, February 2013
-// Version 130316-01
+// Version 130407-01
 //
 // For information on the Pubnub REST API, go here: http://www.pubnub.com/tutorial/http-rest-push-api
 // ******************************************************************************************************************************************************
@@ -17,10 +17,20 @@ using UnityEngine;
 
 public delegate void stringCallback(string message);				// The callback function
 
+
+class MyWebClient : WebClient {										// Increase the number of simultaneous connections allowed
+    protected override WebRequest GetWebRequest(Uri address) {		// to one web resource
+    	HttpWebRequest req = (HttpWebRequest)base.GetWebRequest(address);
+    	req.ServicePoint.ConnectionLimit = 10;
+    	return (WebRequest)req;
+    }
+}
+
+
 public class PubnubThreads
 {
     private string PubnubURL = "http://pubsub.pubnub.com/";			// URL for the Pubnub REST API
-    private Hashtable threadPool = new Hashtable();					// A pool of threads, one for each channel
+    static private Hashtable threadPool = new Hashtable();			// A pool of threads, one for each channel
 
     private string pubnubSubKey = "";
     private string pubnubPubKey = "";
@@ -41,6 +51,7 @@ public class PubnubThreads
         pubnubSubKey = subscribeKey;
         pubnubPubKey = publishKey;
 		pubnubSecretKey = secretKey;
+		
     }
     // ****************************************************************************************************
 
@@ -106,19 +117,18 @@ public class PubnubThreads
     // ****************************************************************************************************
     // Publish a text, Array or Hashtable message to the Pubnub Channel
     // ****************************************************************************************************
-    public void Publish(string channel, object theMessage, stringCallback theCallback)
-    {
-        Thread oThread = new Thread(new ThreadStart(() => { PublishThread(channel, theMessage, theCallback); }));
+    public void Publish(string channel, object theMessage, stringCallback successFunction, stringCallback failureFunction)
+    {	
+        Thread oThread = new Thread(new ThreadStart(() => { PublishThread(channel, theMessage, successFunction, failureFunction); }));
         oThread.Start();
     }
-    private void PublishThread(string channel, object theMessage, stringCallback theCallback)
+	public void PublishThread(string channel, object theMessage, stringCallback successFunction, stringCallback failureFunction)
     {
         string output = "";
-        string queryString = "";
-
+		
         try
         {
-            queryString = "publish/" + pubnubPubKey + "/" + pubnubSubKey + "/0/" + channel + "/0/";
+            string queryString = "publish/" + pubnubPubKey + "/" + pubnubSubKey + "/0/" + channel + "/0/";
             if (theMessage is string)
             {
                 queryString += "\"" + (string)theMessage + "\"";
@@ -131,22 +141,22 @@ public class PubnubThreads
             {
                 queryString += theMessage.ToString();
             }
-
-            WebRequest objRequest = (HttpWebRequest)WebRequest.Create(PubnubURL + queryString);
-
-            WebResponse objResponse = (WebResponse)objRequest.GetResponse();
-            using (StreamReader sr = new StreamReader(objResponse.GetResponseStream()))
-            {
-                output += sr.ReadToEnd();
-                sr.Close();
-            }
+						
+			WebClient client = new MyWebClient ();
+        	client.Headers.Add ("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+			Stream data = client.OpenRead (PubnubURL + queryString);
+        	StreamReader reader = new StreamReader (data);
+			output = reader.ReadToEnd ();
+			data.Close ();
+        	reader.Close ();
+			
+			successFunction(output);
         }
         catch (Exception e)
         {
-            output = "error";
+            output = "error : " + e.Message + " " + e.Source + " " + e.StackTrace;
+			failureFunction(output);
         }
-
-        theCallback(output);
     }
     // ****************************************************************************************************
 
@@ -183,6 +193,7 @@ public class PubnubThreads
         string output = "";
         string queryString = "";
         string timeToken = "0";
+		WebRequest objRequest = null;
 		Debug.Log ("Thread " + channel + " started");
 
         while (!stopped && threadPool.ContainsKey (channel))
@@ -191,21 +202,14 @@ public class PubnubThreads
             {
                 // Create the Query URL
                 queryString = "subscribe/" + pubnubSubKey + "/" + channel + "/0/" + timeToken;
-                WebRequest objRequest = (HttpWebRequest)WebRequest.Create(PubnubURL + queryString);
-			
-				objRequest.Timeout = 10000;
-			
-                // Send the URL and get the Response
-                WebResponse objResponse = (WebResponse)objRequest.GetResponse();
-
-                // Get the Response.  Note that this will pause here if no new messages are waiting, and keep
-                // the http channel open.
-                output = "";
-                using (StreamReader sr = new StreamReader(objResponse.GetResponseStream()))
-                {
-                    output += sr.ReadToEnd();
-                    sr.Close();
-                }
+				
+				WebClient client = new MyWebClient ();
+	        	client.Headers.Add ("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+				Stream data = client.OpenRead (PubnubURL + queryString);
+	        	StreamReader reader = new StreamReader (data);
+				output = reader.ReadToEnd ();
+				data.Close ();
+	        	reader.Close ();
 
                 // Convert it to a form we can work with, namely an Array with the first element an Array of messages
                 // And the second element the timeToken
@@ -238,16 +242,26 @@ public class PubnubThreads
             }
 			catch (WebException w)
 			{
-				//Debug.Log ("Time out");
+				if (objRequest != null) objRequest.Abort ();
+			}
+			catch (ThreadAbortException a)
+			{
+				if (objRequest != null) objRequest.Abort ();
+			}
+			catch (ThreadInterruptedException i)
+			{
+				if (objRequest != null) objRequest.Abort ();
 			}
             catch (Exception e)
             {
-                //theCallback(e.Message);
+				if (objRequest != null) objRequest.Abort ();
 			}
 
             // Give some other threads a chance
             Thread.Sleep(100);
         }
+		
+		if (objRequest != null) objRequest.Abort ();
 		Debug.Log ("Thread " + channel + " stopped");
     }
     // ****************************************************************************************************
@@ -285,6 +299,7 @@ public class PubnubThreads
 			foreach (string theKey in threadPool.Keys)
 	        {
 	            Thread oThread = (Thread)threadPool[theKey];
+				//oThread.Abort();
 	            if (oThread.IsAlive) allStopped = false;
 	        }
 		}
